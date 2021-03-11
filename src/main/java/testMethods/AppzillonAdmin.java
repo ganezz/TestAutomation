76bce69ea65b3b64693dce7f3ba3b84e3b6151cd
sri <sriganesh13091999@gmail.com>
2021-03-11 15:05:49 +0530

    new committed file in Maha sivarathiri day

---
+++ src/main/java/testMethods/AppzillonAdmin.java
@@ -0,0 +1,58 @@
+package testMethods;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+
+
+import static Sync.waitMethods.*;
+import static Utilities.Web.dateSelect;
+import static Utilities.Web.hover;
+
+import static testMethods.WebCapability.driver;
+
+
+public class AppzillonAdmin {
+    public  static  JavascriptExecutor js ;
+    @When("In Login give username as {string} and password as {string} and click login button")
+    public void in_login_give_username_as_and_password_as_and_click_login_button(String username, String password) {
+        driver.manage().window().maximize();
+        driver.findElement(By.id("Admin__Login__userId")).sendKeys(username);
+        driver.findElement(By.id("Admin__Login__pswd")).sendKeys(password);
+        explicit("Admin__Login__element_button_1",8);
+        driver.findElement(By.id("Admin__Login__element_button_1")).click();
+    }
+    @Then("Click the Satff Maintenance in Bank Parameter drop down")
+    public void click_the_satff_maintenance_in_bank_parameter_drop_down() {
+        explicit("Admin__Landing__ct_mnu_2_Bank Parameters_li",8);
+        hover(driver.findElement(By.id("Admin__Landing__ct_mnu_2_Bank Parameters_li")));
+        explicit("Admin__Landing__ct_mnu_2_Staff Maintenance_li",2);
+        driver.findElement(By.id("Admin__Landing__ct_mnu_2_Staff Maintenance_li")).click();
+    }
+    @Then("Give the user id as {string} and click the search button")
+    public void give_the_user_id_as_and_click_the_search_button(String userId) {
+        driver.findElement(By.id("Admin__StaffMaintenanceQuery__userId")).sendKeys(userId);
+        driver.findElement(By.id("Admin__StaffMaintenanceQuery__SearchButton")).click();
+    }
+    @When("Click the table displaying content in user iD column")
+    public void click_the_table_displaying_content_in_user_i_d_column() {
+
+    }
+    @When("in following screen click the unloack button and click the Dateof Birth field")
+    public void in_following_screen_click_the_unloack_button_and_click_the_dateof_birth_field() {
+        driver.findElement(By.xpath("(//tbody/tr/td/a)[1]")).click();
+        driver.findElement(By.id("Admin__StaffMaintenanceDetail__Unlock")).click();
+        driver.findElement(By.id("Admin__StaffMaintenanceEdit__o__tbAsmiUser__DATEOFBIRTH")).click();
+    }
+    @Then("Choose the year as {string} and momth as {string} and Date as {string}")
+    public void choose_the_year_as_and_momth_as_and_date_as(String year, String month, String date) throws InterruptedException {
+        dateSelect(year, month, date);
+    }
+    @Then("Click the save buton")
+    public void click_the_save_buton() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.findElement(By.id("Admin__StaffMaintenanceDetail__Cancel")).click();
+    }
+
+}
